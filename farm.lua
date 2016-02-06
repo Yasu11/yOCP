b5a2a892b2a5b166301937d4e817dc684c81f63e
Yasu11 <wer8023@yahoo.de>
2016-02-07 00:22:33 +0100

    preparations for network attatched farming

--- farm.lua
+++ farm.lua
@@ -1,11 +1,23 @@
-f = require("farmlib")
+require("farmlib")
+k= require("keyboard")
 
 l=9
 w=9
 
-goToStart()
-farmRect(l,w)
-switchField(w)
-farmRect(l,w)
-returnToHome(l)
-dumpInventory()
+stop = false
+
+while true do
+    goToStart()
+    farmRect(l,w)
+    switchField(w)
+    farmRect(l,w)
+    returnToHome(l)
+    for i = 1, 30 do
+        os.sleep(1)
+        if k.isKeyDown(0x2E) and k.isControlDown() then
+            stop = true
+        end
+        if stop then break end
+    end
+    if stop then break end
+end
--- farmlib.lua
+++ farmlib.lua
@@ -2,175 +2,194 @@
 c  = require("component")
 s  = require("sides")
 r  = require("robot")
-r1 = c.proxy(c.list("robot")())
+local r1 = c.robot
 n  = require("note")
 os = require("os")
+t  = require("term")
+m  = c.modem
 
-function sound(a)
-    if a=="error" then
-        n.play("A4",0.2)
-        os.sleep(0.1)
-        n.play("A4",0.2)
-        os.sleep(0.1)
-        n.play("A4",0.2)
-    elseif a == "attention" then
-        n.play("C3",0.3)
-        os.sleep(0.1)
-        n.play("C3",0.3)
-    elseif a == "beep" then
-        n.play("C3",0.1)
-        os.sleep(0.05)
-        n.play("C3",0.1)
-    elseif a == "start" then
-        n.play("C3",0.2)
-        os.sleep(0.05)
-        n.play("E3",0.2)
-        os.sleep(0.05)
-        n.play("G3",0.2)
-        os.sleep(0.05)
-        n.play("C4",0.15)
-    elseif a == "end" then
-        n.play("C4",0.2)
-        os.sleep(0.05)
-        n.play("G3",0.2)
-        os.sleep(0.05)
-        n.play("E3",0.2)
-        os.sleep(0.05)
-        n.play("C3",0.15)
-    end
+function getInvRat()
+	iS = r.inventorySize()
+	iL = 0
+	for i = 1, iS do
+		if r.count(i) > 0 then
+			iL = iL +1
+		end
+	end
+	return tostring(iL/iS)
 end
 
-function forward ()
+function sound(a)
+	if a=="error" then
+		n.play("A4",0.2)
+		os.sleep(0.1)
+		n.play("A4",0.2)
+		os.sleep(0.1)
+		n.play("A4",0.2)
+	elseif a == "attention" then
+		n.play("C3",0.3)
+	elseif a == "beep" then
+		n.play("C3",0.1)
+		os.sleep(0.05)
+		n.play("C3",0.1)
+	elseif a == "start" then
+		n.play("C3",0.2)
+		os.sleep(0.05)
+		n.play("E3",0.2)
+		os.sleep(0.05)
+		n.play("G3",0.2)
+		os.sleep(0.05)
+		n.play("C4",0.15)
+	elseif a == "end" then
+		n.play("C4",0.2)
+		os.sleep(0.05)
+		n.play("G3",0.2)
+		os.sleep(0.05)
+		n.play("E3",0.2)
+		os.sleep(0.05)
+		n.play("C3",0.15)
+	end
+end
+
+local function throwError()
+	sound("error")
+end
+
+local function forward ()
 	b = false
+	i = 0
 	while true do
 		b = r.forward()
 		if b then
 			return
 		end
-        sound("beep")
-		os.sleep(0.2)
+		sound("beep")
+		os.sleep(0.25)
+		i = i + 1
+		if i > 20 then
+			throwError()
+			i = 0
+		end
 	end
 end
 
-function farm ()
-    r1.use(s.bottom)
+local function farm ()
+	r1.use(s.bottom)
 end
 
 function farmLine (length)
-    if length < 1 then 
-        return
-    end
-    for i=2, length do
-        farm()
-        forward()
-    end
-    farm()
+    t.write(getInventoryLevel())
+	if length < 1 then
+		return
+	end
+	for i=2, length do
+		farm()
+		forward()
+	end
+	farm()
+    m.broadcast(1111,tostring(getInventoryLevel()))
 end
 
 function switchLeft()
-    sound("attention")
-    r.turnLeft()
-    forward()
-    r.turnLeft()
+	sound("attention")
+	r.turnLeft()
+	forward()
+	r.turnLeft()
 end
 
 function switchRight()
-    sound("attention")
-    r.turnRight()
-    forward()
-    r.turnRight()
+	sound("attention")
+	r.turnRight()
+	forward()
+	r.turnRight()
 end
 
 -- farms a field from the lower left
 -- ends on upper right
 function farmRect(length, width)
-    if width < 1 then
-        return
-    end
-    
-    farmLine(length)
-    width=width-1
-    
-    for i=1, width do
-        if (i % 2) == 1 then
-            switchRight()
-        else
-            switchLeft()
-        end
-        farmLine(length)
-    end    
-    
-    if width % 2 == 0 then
-    r.turnAround()        
-        for i=1, length do
-            forward()
-        end
-    end
+	if width < 1 then
+		return
+	end
+
+	farmLine(length)
+	width=width-1
+
+	for i=1, width do
+		if (i % 2) == 1 then
+			switchRight()
+		else
+			switchLeft()
+		end
+		farmLine(length)
+	end
+
+	if width % 2 == 1 then
+	r.turnAround()
+		for i=1, length do
+			forward()
+		end
+	end
 end
 
 function switchField(width)
-    sound("attention")
-    r.up()
-    forward()
-    forward()
-    forward()
-    r.down()
-    r.turnLeft()
-    for i=1, width-1 do
-        forward()
-    end
-    r.turnRight()
+	sound("attention")
+	r.up()
+	forward()
+	forward()
+	forward()
+	r.down()
+	r.turnLeft()
+	for i=1, width-1 do
+		forward()
+	end
+	r.turnRight()
 end
 
-function dumpInventory()
-    os.sleep(20)
-end
-
--- goes to the lower left 
+-- goes to the lower left
 -- of the first field
 function goToStart()
-    sound("start")
-    forward()
-    forward()
-    r.down()
-    r.down()
-    r.down()
-    r.turnLeft()
-    forward()
-    forward()
-    forward()
-    forward()
-    r.turnRight()
+	sound("start")
+	forward()
+	forward()
+	r.down()
+	r.down()
+	r.down()
+	r.turnLeft()
+	forward()
+	forward()
+	forward()
+	forward()
+	r.turnRight()
 end
 
 function returnToHome(length)
-    r.turnAround()
-    for i=1, length-1 do
-        forward()
-    end
-    
-    sound("attention")
-    r.up()
-    forward()
-    forward()
-    forward()
-    r.down()
-    
-    for i=1, length-1 do
-        forward()
-    end
-    r.turnRight()
-    for i=1, length/2 do
-        forward()
-    end
-    r.turnLeft()
-    r.up()
-    forward()
-    r.up()
-    r.up()
-    forward()
-    r.turnAround()
-    sound("end")
+	r.turnAround()
+	for i=1, length-1 do
+		forward()
+	end
+
+	sound("attention")
+	r.up()
+	forward()
+	forward()
+	forward()
+	r.down()
+
+	for i=1, length-1 do
+		forward()
+	end
+	r.turnRight()
+	for i=1, length/2 do
+		forward()
+	end
+	r.turnLeft()
+	r.up()
+	forward()
+	r.up()
+	r.up()
+	forward()
+	r.turnAround()
+	sound("end")
 end
 
 -- EOF
