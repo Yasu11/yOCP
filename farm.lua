2e5da83fd3018290d3de875004fe9675c8dc6b56
Yasu11 <wer8023@yahoo.de>
2016-02-06 22:20:47 +0100

    sound improvement and bugfix for farm.lua

--- farm.lua
+++ farm.lua
@@ -3,9 +3,9 @@ f = require("farmlib")
 l=9
 w=9
 
-f.goToStart()
-f.farmRect(l,w)
-f.switchField(w)
-f.farmRect(l,w)
-f.returnToHome(l)
-f.dumpInventory()
+goToStart()
+farmRect(l,w)
+switchField(w)
+farmRect(l,w)
+returnToHome(l)
+dumpInventory()
